d05737bf9afa421013a01841ae74005f143b9872
glennosborne <glenn@theosbornes.org>
2026-01-05 07:44:34 -0500

    Update copyrights

--- torqlang-klvm/src/main/java/org/torqlang/klvm/CompleteOrIdent.java
+++ torqlang-klvm/src/main/java/org/torqlang/klvm/CompleteOrIdent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Torqware LLC. All rights reserved.
+ * Copyright (c) 2024-2026 Torqware LLC. All rights reserved.
  *
  * You should have received a copy of the Torq Lang License v1.0 along with this program.
  * If not, see <http://torq-lang.github.io/licensing/torq-lang-license-v1_0>.
